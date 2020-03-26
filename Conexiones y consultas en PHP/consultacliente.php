498cd8354fb19f731ce3872cac46e1dfd35bfa20
JesusAngel22 <60763852+JesusAngel22@users.noreply.github.com>
2020-03-25 23:39:42 -0600

    Conexión y consulta en php

---
+++ Conexiones y consultas en PHP/consultacliente.php
@@ -0,0 +1,34 @@
+<?php
+$host ="localhost";
+$user ="root";
+$pass ="";
+$db='consultaprueba';
+$con=mysqli_connect($host,$user,$pass)or die("Error al Conectar");
+mysqli_select_db($con,$db)or die("No de pudo conectar con la base de datos ERROR 404 NOT FOUND");
+
+$cons=$_POST["consulta"];
+$r=$_POST["nrandom2"];
+if ($cons == "cliente"){
+	$sql = "select * from cliente where nombre = '$r'";
+    $consresult = mysqli_query($con, $sql);
+    $h = mysqli_num_rows($consresult);
+	if ($h==0){
+		echo "Error al encontrar resultados intente de nuevo";
+		}else{
+			while ( $row = mysqli_fetch_assoc($consresult)) {
+                echo "<strong>nombre:</strong> ";
+                echo $row["nombre"]."<br><br>";
+                echo "<strong>Primer Apellido:</strong> ";
+				echo $row["primerapellido"]."<br><br>";
+				echo "<strong>Segundo Apellido:</strong> ";
+				echo $row["segundoapellido"]."<br><br>";
+				echo "<strong>Edad:</strong> ";
+				echo $row["edad"]."<br><br>";
+				echo "<strong>Telefono:</strong> ";
+				echo $row["celular"]."<br><br>";
+				echo "<strong>Sucursal a la que asiste:</strong> ";
+				echo $row["sucursalcliente"]."<br>";
+		}
+	}
+}
+?>
---
+++ Conexiones y consultas en PHP/consultasucursal.php
@@ -0,0 +1,28 @@
+<?php
+$host ="localhost";
+$user ="root";
+$pass ="";
+$db='consultaprueba';
+$con=mysqli_connect($host,$user,$pass)or die("Error al Conectar");
+mysqli_select_db($con,$db)or die("No de pudo conectar con la base de datos ERROR 404 NOT FOUND");
+
+$cons=$_POST["consulta"];
+$r=$_POST["nrandom1"];
+if ($cons == "sucursal"){
+	$sql = "select nombre,primerapellido from cliente where sucursalcliente = '$r'";
+    $consresult = mysqli_query($con, $sql);
+    $h = mysqli_num_rows($consresult);
+	if ($h==0){
+		echo "No se Encontro reultado";
+		}else{
+		echo "<strong>Clientes de la sucursal:</strong> ", $r."<br><br>";
+		while ( $row = mysqli_fetch_assoc($consresult)) {
+            echo $row["nombre"]."<br>";
+			echo $row["primerapellido"]."<br><br>";
+
+		}
+	}
+    }else{
+    echo "Error 404 NOT FOUND xd";
+	}
+?>
